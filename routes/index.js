d825c0b11163fdb0ee2a54ccf6cf777412637f4b
cgenin <genin.christophe@gmail.com>
2019-05-26 12:38:42 +0200

    Ajout du mode history

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,26 @@
-var express = require('express');
-var router = express.Router();
+const fs = require('fs');
+const express = require('express');
+const router = express.Router();
 
 /* GET home page.
     router.get('/', function(req, res, next) {
       res.render('index', { title: 'Express' });
     });
+
 */
 
+router.get('/*', (req, res) => {
+    fs.readFile('dist/index.html', 'utf-8', (err, content) => {
+        if (err) {
+            console.error('We cannot open "index.html" file.')
+        }
+
+        res.writeHead(200, {
+            'Content-Type': 'text/html; charset=utf-8'
+        })
+
+        res.end(content)
+    });
+});
 
 module.exports = router;
